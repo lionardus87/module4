849777b75b63a68269f0df4801b77e8717ef9a03
lionardus87 <yudhi_lee@hotmail.com>
2025-03-28 22:41:11 +1100

    labexercise5

--- lab exercise 5/index.js
+++ lab exercise 5/index.js
@@ -4,10 +4,24 @@ let news = [
 	{ id: 3, title: "Tornado Warning", content: "Residents should prepare..." },
 ];
 
-function newsFeed() {
-	return new Promise((resolve) => {
-		setInterval(function () {
-			resolve(news);
-		}, 5000);
+function updateNews() {
+	const template = document.getElementById("newsContainer");
+	// template.innerHTML = "";
+	news.forEach((item) => {
+		const article = document.createElement("div");
+		article.classList.add("news-item");
+		article.innerHTML = `<h2>${item.title}</h2><p>${item.content}</p>`;
+		template.appendChild(article);
 	});
 }
+function startNews() {
+	intervalId = setInterval(updateNews, 5000);
+}
+
+function stopNews() {
+	clearInterval(intervalId);
+}
+
+startNews();
+
+document.getElementById("stopButton").addEventListener("click", stopNews);
